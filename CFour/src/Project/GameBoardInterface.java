500098d27be1efd52527878fc58a7b2d87207ed8
asifakhan00011 <44386434+asifakhan00011@users.noreply.github.com>
2018-10-23 23:14:15 -0400

    Add files via upload

---
+++ CFour/src/Project/GameBoardInterface.java
@@ -0,0 +1,11 @@
+package Project;
+
+public interface GameBoardInterface {
+public void displayBoard();
+public void clearBoard();
+public boolean isEmpty();
+public boolean isFull();
+public void showScore();
+public void playAgain();
+
+}
